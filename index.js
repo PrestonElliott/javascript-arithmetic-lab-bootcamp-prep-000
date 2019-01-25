e618d9e62e87922b61b95467c6d7e872a91609d1
Preston Elliott <preston.elliott24@gmail.com>
2019-01-25 17:34:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,53 @@
+var a = 900;
+var b = 75;
+
+function add(a, b) {
+  return(a + b);
+  }
+add();
+
+function subtract(a, b) {
+  return(a - b);
+  } 
+subtract();
+
+function multiply(a, b) {
+  return(a * b);
+  }
+multiply();
+
+function divide(a, b) {
+  return(a / b);
+  }
+divide();
+
+var n = 81;
+
+function inc(n) {
+  return(n + 1);
+  }
+inc();
+
+function dec(n) {
+  return(n - 1);
+  }
+dec();
+
+function makeInt(n) {
+  return parseInt('n', 10);
+  }
+makeInt();
+
+var n = 941.293;
+
+function makeInt(n) {
+   return parseInt('n', 10);
+  }
+makeInt();
+
+
+
+
+
+
+
